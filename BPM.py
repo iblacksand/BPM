822e8e6040b106036a6003aa596f8cfa881f9951
John Elizarraras <elizarrj27@gmail.com>
2021-04-12 15:17:32 -0500

    adds docstrings to all of the functions

--- BPM.py
+++ BPM.py
@@ -18,8 +18,10 @@ board = 0
 @click.option('--frequency', '-f', 'f', default=20, show_default=True, help='Data acquisition frequency.')
 @click.option('--save', '-s', 's', is_flag=True, help='save the data after processing')
 @click.option('--show-autocorrelation', '-showa', 'showa', is_flag=True, help='Show the results of autocorrelation')
-@click.option('--port', '-p', 'port', default='COM4', prompt='Arduino Port')
-def start(t1, cycle, b, f, s, showa, port):
+@click.option('--port', '-p', 'port', default='COM4', show_default=True, prompt='Arduino Port', help='The port of the Arduino.')
+def start(t1, cycle, b, f, s, showa, port): 
+    '''Runs the data collection and calculations
+    ''' 
     board = pyfirmata.Arduino(port)
     it = pyfirmata.util.Iterator(board)
     it.start()
@@ -47,8 +49,16 @@ def start(t1, cycle, b, f, s, showa, port):
     if s:
         save(mass_data)
     
-
 def gather(cycle, f):
+    """gathers voltage readings for the specified amount of time
+
+    Args:
+        cycle (int): length of time to run the cycle
+        f (int): frequency of collection
+
+    Returns:
+        double array: array of the voltages collected at the frequency rate
+    """    
     tick = 1/f
     data = []
     for i in range(0, ceil(cycle/tick)):
@@ -58,6 +68,16 @@ def gather(cycle, f):
     return data
 
 def calculate(data, f, showa):
+    """calculates the beats per minute of a provided signal
+
+    Args:
+        data (double array): array of voltages to calculate bpm from
+        f (frequency): frequency used to collect the data
+        showa (boolean): whether or not to show the graph of the autocorrelation
+
+    Returns:
+        double: the beats per minute of the signal
+    """    
     x = autocorr(data)
     peaks = find_peaks(x, prominence=1)
     bpms = []
@@ -67,12 +87,25 @@ def calculate(data, f, showa):
     return bpm
 
 def save(data):
+    """saves the given data into a csv file
+
+    Args:
+        data (double array): array to save into a csv file
+    """    
     files = [ ('Comma-separated values', '*.csv')]
     file = asksaveasfile(filetypes = files, defaultextension = files)
     a = np.asarray(data)
     a.tofile(file.name,sep=',',format='%10.5f')
 
 def autocorr(x):
+    """runs autocorrelation on the provided array
+
+    Args:
+        x (double array): array to run autocorrelation on
+
+    Returns:
+        double array: results of the autocorrelation
+    """
     result = np.correlate(x, x, mode='full')
     return result[result.size/2:]
 
